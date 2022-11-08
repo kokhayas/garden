9ce6365c1b30329fd002d957df7bda9eb9e3c752
kokhayas <58214906+kokhayas@users.noreply.github.com>
2022-11-08 23:51:36 +0900

    Update nine_block_analyzer.py

--- Zipc_airflow/src/analyzer/nine_block_analyzer.py
+++ Zipc_airflow/src/analyzer/nine_block_analyzer.py
@@ -1,94 +1,111 @@
+import datetime
 import string
 from dataclasses import dataclass
 from enum import Enum
-from typing import List, NewType, Optional, TypedDict, Union
+from typing import List, NewType, Optional, Tuple, TypedDict, Union
 
 from base_analayzer import BaseAnalyzer
 from datas.lane_data import *
 from utils.rdf_graph_creator import RdfGraphCreator
 from utils.scanner import Scanner
 
-
-class ImportedData(TypedDict):
-	measurement: string
-	mapid: int
-	imported_data_id: int
-	status: int
+from Zipc_airflow.src.analyzer.my_dataclass import (Measurement,
+                                                    NineBlockAnalyzerData,
+                                                    Vehicles)
 
 
+@dataclass
+class ImportedData():
+    measurement: string
+    mapid: int
+    imported_data_id: int
+    status: int
 
-class Connection(TypedDict):
-	type: string # type takes string "successor" or "predecessor"
-	road: string
-	lane: string
+@dataclass
+class Connection():
+    type: string # type takes string "successor" or "predecessor"
+    road: string
+    lane: string
 
-class Point(TypedDict):
-	x: float
-	y: float
-	z: float
-	roll: float
-	pitch: float
-	yaw: float
+@dataclass
+class Point():
+    x: float
+    y: float
+    z: float
+    roll: float
+    pitch: float
+    yaw: float
 
-class Waypoint_(TypedDict):
-	index: int
-	point: Point
+@dataclass
+class Waypoint_():
+    index: int
+    point: Point
+
+@dataclass
+class Lane():
+    name: string
+    width: int
+    connection: List[Connection]
+    waypoints: List[Waypoint_]
+
+@dataclass
+class Road():
+    name: string
+    lanes: List[Lane]
+
+@dataclass
+class WaypointData():
+    gid: string
+    direction: string  # "left" or "right"
+    roads: List[Road]
+
+@dataclass
+class _Type():
+    road: string
+    lane: string
+
+@dataclass
+class Lane_Dict():
+    successor: _Type
+    predecessor: _Type
+
+@dataclass
+class Road_Dict():
+    index: int
+    lane_name: Lane_Dict
+
+@dataclass
+class Data_Dict():
+    road_name: Road_Dict
+
+@dataclass
+class _Vehicle():
+    direction: string  # DirectionType.*.value
+    position: int   #おそらく　egoが0, frontが1, frontfrontが2, backが1, backbackが2となる
+
+@dataclass
+class Time_Dict():
+    vehicle: _Vehicle  # 複数の種類の名前のvehicleがある
+
+@dataclass
+class Vehicle_Dict():
+    time: Time_Dict
+
+@dataclass
+class NineBlockAnalyzerData():
+    time: datetime.datetime
+    ego_road: string
+    ego_lane: string
+    ego_waypoint_index: int
+    vehicle_road: string
+    vehicle_lane: string
+    vehicle_waypoint_index: int
 
 
-class Lane(TypedDict):
-	name: string
-	width: int
-	connection: List[Connection]
-	waypoints: List[Waypoint_]
 
-class Road(TypedDict):
-	name: string
-	lanes: List[Lane]
-
-class WaypointData(TypedDict):
-	gid: string
-	direction: string  # "left" or "right"
-	roads: List[Road]
-
-
-class _Type(TypedDict):
-	road: string
-	lane: string
-
-class Lane_Dict(TypedDict):
-	successor: _Type
-	predecessor: _Type
-
-class Road_Dict(TypedDict):
-	index: int
-	lane_name: Lane_Dict
-
-class Data_Dict(TypedDict):
-	road_name: Road_Dict
-
-
-class _Vehicle(TypedDict):
-	direction: string
-	position: int
-
-class Time_Dict(TypedDict):
-	vehicle: _Vehicle
-
-class Vehicle_Dict(TypedDict):
-	time: Time_Dict
-
-class Data(TypedDict):
-	time: int
-	ego_road: string
-	ego_lane: string
-	ego_waypoint_index: string
-	vehicle_road: string
-	vehicle_lane: string
-	vehicle_waypoint_index: string
-
-
-
-
+@dataclass
+class Rdf_Data():
+	vehicle: List[SceneData] # SceneDataには、start, end, behaviorTypeが記載されている
 
 
 from datas.behaivor_type import BehaviorType
@@ -103,7 +120,7 @@ from datas.waypoint import Waypoint
 
 class NineBlockAnalyzer(BaseAnalyzer):
 
-    def road_lane_check(self, diff_pos, diff_lane):
+    def road_lane_check(self, diff_pos: int, diff_lane: int) -> str:
         """
 
         :param diff_pos:
@@ -183,39 +200,39 @@ class NineBlockAnalyzer(BaseAnalyzer):
         return data_dict
 
     def get_nine_block_data(self, measurement: string, road_data_dict: Data_Dict, vehicles: List[str]) -> Vehicle_Dict:
-        vehicle_dict = {}
+        vehicle_dict: Vehicle_Dict = {}
         for vehicle in vehicles:
             if vehicle == "ego":
                 continue
-            col_ego_road = "ego_road"
-            col_ego_lane = "ego_lane"
-            col_ego_waypoint_index = "ego_waypoint_index"
-            col_obs_road = "{}_road".format(vehicle)
-            col_obs_lane = "{}_lane".format(vehicle)
-            col_obs_waypoint_index = "{}_waypoint_index".format(vehicle)
-            query = 'select {}, {}, {}, {}, {}, {} from "{}"'.format(col_ego_road,
+            col_ego_road: str = "ego_road"
+            col_ego_lane: str = "ego_lane"
+            col_ego_waypoint_index: str = "ego_waypoint_index"
+            col_obs_road: str = "{}_road".format(vehicle)
+            col_obs_lane: str = "{}_lane".format(vehicle)
+            col_obs_waypoint_index: str = "{}_waypoint_index".format(vehicle)
+            query : str= 'select {}, {}, {}, {}, {}, {} from "{}"'.format(col_ego_road,
                                                                      col_ego_lane,
                                                                      col_ego_waypoint_index,
                                                                      col_obs_road,
                                                                      col_obs_lane,
                                                                      col_obs_waypoint_index,
                                                                      measurement)
-            datas = self.influxdb_accessor.query(query)
+            datas: List[NineBlockAnalyzerData] = self.influxdb_accessor.query(query)
             if datas is None:
                 continue
             for data in datas:
-                time = data['time']
-                time_dict = vehicle_dict.get(time)
+                time: datetime.datetime  = data['time']
+                time_dict: Time_Dict = vehicle_dict.get(time)
                 if time_dict is None:
                     time_dict = {}
                     vehicle_dict[time] = time_dict
 
                 ego_road: str = data[col_ego_road]
                 ego_lane: str = data[col_ego_lane]
-                ego_index: str = data[col_ego_waypoint_index]
+                ego_index: int = data[col_ego_waypoint_index]
                 obs_road: str = data[col_obs_road]
                 obs_lane: str = data[col_obs_lane]
-                obs_index: str = data[col_obs_waypoint_index]
+                obs_index: int = data[col_obs_waypoint_index]
 
                 if not self.direction_check(ego_lane, obs_lane):
                     # 進行方向が逆のため、評価不要
@@ -225,65 +242,65 @@ class NineBlockAnalyzer(BaseAnalyzer):
                     continue
 
                 # 自車両より前を評価
-                scanner = Scanner(road_data_dict, obs_road, obs_lane)
+                scanner: Scanner = Scanner(road_data_dict, obs_road, obs_lane)
                 index, lane_diff = scanner.successor(ego_road, ego_lane)
                 diff_pos = None
                 if not (index is None or lane_diff is None):
                     # 横または前方（Road跨ぎ含む）
-                    diff_pos = (index + obs_index) - ego_index
-                    result = self.road_lane_check(diff_pos, lane_diff)
+                    diff_pos: int = (index + obs_index) - ego_index
+                    result: DirectionType = self.road_lane_check(diff_pos, lane_diff)
                 else:
                     # 後方（Road跨ぎ）
                     index, lane_diff = scanner.predecessor(ego_road, ego_lane)
                     if not (index is None or lane_diff is None):
                         # 後方（Road跨ぎ）
-                        diff_pos = (index - obs_index) + ego_index
-                        result = self.road_lane_check(diff_pos, lane_diff)
+                        diff_pos: int = (index - obs_index) + ego_index
+                        result: DirectionType = self.road_lane_check(diff_pos, lane_diff)
 
-                time_dict[vehicle] = {"direction": result if result is None else result.value,
+                time_dict[vehicle]: _Vehicle = {"direction": result if result is None else result.value,
                                       "position": diff_pos
                                       }
         return vehicle_dict
 
-    def nine_block_classification(self, time_series_vehicle_dict):
+    def nine_block_classification(self, time_series_vehicle_dict: Vehicle_Dict) -> Vehicle_Dict: # 減量したVehicle_Dictが返される(Vehicle_dict.timeからFrontFront以外が削除される)
         """
         FrontFront, Backの解析
-　        FrontFront:obsB
-  　      　direction ->
-    　    　　ego - obsA - obsB - obsC
-      　  Back:obsB
-        　　direction ->
-        　　　obsA - obsB - ego - obsC
+        FrontFront:obsB
+        direction ->
+            ego - obsA - obsB - obsC
+            Back:obsB
+            direction ->
+            obsA - obsB - ego - obsC
         :param time_series_vehicle_dict:
         :return:
         """
-        for time, vehicle_dict in time_series_vehicle_dict.items():
-            front_dict = {}
-            back_dict = {}
-            for vehicle, data in vehicle_dict.items():
+        for time, vehicle_dict in time_series_vehicle_dict.items():  # time, Time_Dict
+            front_dict: Time_Dict = {}
+            back_dict : Time_Dict = {}
+            for vehicle, data in vehicle_dict.items():  # vehicle, _Vehicle
                 if data.get('direction') == DirectionType.FRONT.value:
                     front_dict[vehicle] = data
                 elif data.get('direction') == DirectionType.BACK.value:
                     back_dict[vehicle] = data
             # Front解析
-            sorted_front = sorted(front_dict.items(), key=lambda x: x[1].get('position'))
-            sorted_back = sorted(back_dict.items(), key=lambda x: x[1].get('position'))
+            sorted_front: List[Tuple[str, _Vehicle]] = sorted(front_dict.items(), key=lambda x: x[1].get('position')) # Time_Dict.items()　# positionでソート
+            sorted_back: List[Tuple[str, _Vehicle]] = sorted(back_dict.items(), key=lambda x: x[1].get('position'))
 
             # egoの2つ前の車両をFrontFrontにし、以降は排除
             for num in range(1, len(sorted_front)):
-                data = sorted_front[num]
+                data: Tuple[str, _Vehicle] = sorted_front[num]
                 if num == 1:
-                    vehicle_data = vehicle_dict.get(data[0])
+                    vehicle_data: _Vehicle = vehicle_dict.get(data[0]) # data[0] は　Time_Dictのvehicle
                     vehicle_data['direction'] = DirectionType.FRONT_FRONT.value
                 else:
                     vehicle_dict.pop(data[0])
             # egoの直後以降の他車両を排除
             for num in range(1, len(sorted_back)):
-                data = sorted_back[num]
+                data: Tuple[str, _Vehicle] = sorted_back[num]
                 vehicle_dict.pop(data.key())
         return time_series_vehicle_dict
 
-    def create_direction_data(self, direction, time):
+    def create_direction_data(self, direction: DirectionType, time: str):
         """
 
         :param direction:
@@ -311,18 +328,18 @@ class NineBlockAnalyzer(BaseAnalyzer):
             data = RightBackData(start=time)
         return data
 
-    def create_rdf_data(self, time_series_vehicle_dict, vehicles):
-        rdf_data = {}
+    def create_rdf_data(self, time_series_vehicle_dict: Vehicle_Dict, vehicles: List[str]) -> Rdf_Data:
+        rdf_data: Rdf_Data = {}
         for vehicle in vehicles:
             if vehicle == "ego":
                 continue
             vehicle_rdf_data_list = rdf_data.get(vehicle)
             if vehicle_rdf_data_list is None:
-                vehicle_rdf_data_list = []
+                vehicle_rdf_data_list: List[SceneData] = []
                 rdf_data[vehicle] = vehicle_rdf_data_list
 
-            prev_time = None
-            direction_data = None
+            prev_time: Optional[str] = None
+            direction_data: SceneData = None
             for time, time_series_data in time_series_vehicle_dict.items():
                 vehicle_data = time_series_data.get(vehicle)
                 if vehicle_data is None:
@@ -331,10 +348,10 @@ class NineBlockAnalyzer(BaseAnalyzer):
                         vehicle_rdf_data_list.append(direction_data)
                         direction_data = None
                 else:
-                    direction = vehicle_data.get('direction')
+                    direction: DirectionType = vehicle_data.get('direction')
 
                     if direction_data is None:
-                        direction_data = self.create_direction_data(direction, time)
+                        direction_data = self.create_direction_data(direction, time) # SceneData
 
                     elif not direction_data.equals(direction):
                         direction_data.set_end(prev_time)
@@ -354,7 +371,7 @@ class NineBlockAnalyzer(BaseAnalyzer):
         """
         # 走行データ管理DBからレコードを取得
         imported_data: ImportedData = self.get_imported_data(imported_data_id)
-        measurement: string = imported_data.measurement
+        measurement: Measurement = imported_data.measurement
         map_id: int = imported_data.mapid
 
         # waypointsから対象のwaypointを取得
@@ -364,21 +381,22 @@ class NineBlockAnalyzer(BaseAnalyzer):
         road_data_dict: Data_Dict = self.get_road_info(waypoint_data)
 
         # 車両取得
-        vehicles: List[str] = self.influxdb_accessor.get_vehicles(measurement)
+        vehicles: Vehicles = self.influxdb_accessor.get_vehicles(measurement)
 
         # 車両ごとの走行Lane情報を取得
-        time_series_vehicle_dict = self.get_nine_block_data(measurement, road_data_dict, vehicles)
+        time_series_vehicle_dict: Vehicle_Dict = self.get_nine_block_data(measurement, road_data_dict, vehicles)
 
         # FrontFront, Backの分類
-        time_series_vehicle_dict = self.nine_block_classification(time_series_vehicle_dict)
+        time_series_vehicle_dict: Vehicle_Dict = self.nine_block_classification(time_series_vehicle_dict)
 
-        rdf_data = self.create_rdf_data(time_series_vehicle_dict, vehicles)
+        rdf_data: Rdf_Data = self.create_rdf_data(time_series_vehicle_dict, vehicles)
 
         # RDFへ書き込み
-        creator = RdfGraphCreator("garden_ts", measurement)
-        creator.build_direction(rdf_data, "ego")
+        creator : RdfGraphCreator = RdfGraphCreator("garden_ts", measurement)
+        creator.build_direction(rdf_data, "ego") # creator.build_direction(rdf_data, ego, SceneType.DIRECTION)
 
 
+# ひとことメモ　nine_block_analyzerは、Rdf_Dataを作成している。vehicle: List[SceneData], SceneDataには、DirectionType(9種類(frontfront, front, back, leftfront, left, leftback, rightfront, right, rightback)), start, endがある。
 
 def execute(**kwargs):
     """
